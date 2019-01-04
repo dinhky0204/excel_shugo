0f2c6a8fa36d381d526f4528513ae790010c6767
dinhky <n3835@ndensan.co.jp>
2019-01-04 14:31:16 +0900

    集合ファイルを作成

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,23 +33,23 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(307, 135);
+            this.button1.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(145, 46);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 54);
+            this.button1.Size = new System.Drawing.Size(141, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Choose File";
+            this.button1.Text = "フォルダ選択";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(429, 225);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "フォルダ選択";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -14,6 +14,8 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        static int x = 200;
+        static int y = 200;
         protected Excel.Application xlApp = new Excel.Application();
         private List<Training> objectList = new List<Training>();
         public Form1()
@@ -50,6 +52,7 @@ namespace WindowsFormsApp1
             Training info = new Training();
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(selectedPath + "/" + @file.Name);
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            object misValue = System.Reflection.Missing.Value;
 
             Excel.Range xlRange = xlWorksheet.UsedRange;
             int rowCount = xlRange.Rows.Count;
@@ -61,7 +64,7 @@ namespace WindowsFormsApp1
 
             info.CourseNumber = getTextDataFromRange(xlRange, 8, 3);
             info.CourseName = getTextDataFromRange(xlRange, 9, 3);
-            info.CourseNumber = getTextDataFromRange(xlRange, 10, 3);
+            info.Department = getTextDataFromRange(xlRange, 10, 3);
             info.StaffCode = getTextDataFromRange(xlRange, 11, 3);
             info.StaffName = getTextDataFromRange(xlRange, 12, 3);
 
@@ -81,14 +84,93 @@ namespace WindowsFormsApp1
             info.Day_reason = getTextDataFromRange(xlRange, 26, 6);
             info.Condition_reason = getTextDataFromRange(xlRange, 27, 6);
 
-            xlWorkbook.Close();
+            xlWorkbook.Close(false, misValue, misValue);
 
             return info;
+        }
+
+        public int setStatus(Boolean tmp)
+        {
+            if (tmp == true) return 1;
+            else return 2;
+        }
+        
+        private void writeExcelFile(String fileName, List<Training> dataList) 
+        {
+            xlApp.StandardFont = "游ゴシック";
+            xlApp.StandardFontSize = 10;
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Add(Type.Missing);
+
+            Excel._Worksheet xlWorkSheet = (Excel._Worksheet) xlWorkBook.ActiveSheet;  
+            
+            //Set workSheet Info
+            xlWorkSheet.Name = "合計";
+               
+            xlWorkSheet.Cells[1, 1] = "No.";
+            xlWorkSheet.Cells[1, 2] = "研修日";
+            xlWorkSheet.Cells[1, 3] = "コースNo.";
+            xlWorkSheet.Cells[1, 4] = "研修名";
+            xlWorkSheet.Cells[1, 5] = "署名";
+            xlWorkSheet.Cells[1, 6] = "社員番号";
+            xlWorkSheet.Cells[1, 7] = "氏名";
+            
+            xlWorkSheet.Cells[1, 8] = "講師";
+            xlWorkSheet.Cells[1, 9] = "テキスト";
+            xlWorkSheet.Cells[1, 10] = "内容";
+            xlWorkSheet.Cells[1, 11] = "継続有無";
+            xlWorkSheet.Cells[1, 12] = "理由：講師";
+            xlWorkSheet.Cells[1, 13] = "理由：テキスト";
+            xlWorkSheet.Cells[1, 14] = "理由：内容";
+            xlWorkSheet.Cells[1, 15] = "理由：継続有無";
+
+            xlWorkSheet.Cells[1, 16] = "時期";
+            xlWorkSheet.Cells[1, 17] = "日数";
+            xlWorkSheet.Cells[1, 18] = "対象者条件";
+            xlWorkSheet.Cells[1, 19] = "理由：時期";
+            xlWorkSheet.Cells[1, 20] = "理由：日数";
+            xlWorkSheet.Cells[1, 21] = "理由：対象者条件";
+
+            //Write data into excel File
+            int i = 2;
+            foreach(Training dataInfo in dataList) 
+            {
+                xlWorkSheet.Cells[i, 1] = i-1;
+                xlWorkSheet.Cells[i, 2] = dataInfo.KenshuuDate;
+                xlWorkSheet.Cells[i, 3] = dataInfo.CourseNumber;
+                xlWorkSheet.Cells[i, 4] = dataInfo.CourseName;
+                xlWorkSheet.Cells[i, 5] = dataInfo.Department;
+                xlWorkSheet.Cells[i, 6] = dataInfo.StaffCode;
+                xlWorkSheet.Cells[i, 7] = dataInfo.StaffName;
+                
+                xlWorkSheet.Cells[i, 8] = setStatus(dataInfo.Lecturer);
+                xlWorkSheet.Cells[i, 9] = setStatus(dataInfo.Text);
+                xlWorkSheet.Cells[i, 10] = setStatus(dataInfo.Content);
+                xlWorkSheet.Cells[i, 11] = setStatus(dataInfo.Continuation);
+                xlWorkSheet.Cells[i, 12] = dataInfo.Lecturer_reason;
+                xlWorkSheet.Cells[i, 13] = dataInfo.Text_reason;
+                xlWorkSheet.Cells[i, 14] = dataInfo.Content_reason;
+                xlWorkSheet.Cells[i, 15] = dataInfo.Continuation_reason;
+                              
+                xlWorkSheet.Cells[i, 16] = setStatus(dataInfo.Time);
+                xlWorkSheet.Cells[i, 17] = setStatus(dataInfo.Day);
+                xlWorkSheet.Cells[i, 18] = setStatus(dataInfo.Condition);
+                xlWorkSheet.Cells[i, 19] = dataInfo.Time_reason;
+                xlWorkSheet.Cells[i, 20] = dataInfo.Day_reason;
+                xlWorkSheet.Cells[i, 21] = dataInfo.Condition_reason;
+
+                ++i;
+            }
+
+
+
+            xlWorkBook.SaveAs(@"C:\Users\n3835\Desktop\キー\" + fileName);
+            xlWorkBook.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
+            String fileName = DateTime.Now.ToString("dd_MM_yyyy_HHmmss") + ".xlsx";
             if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 DirectoryInfo d = new System.IO.DirectoryInfo(fbd.SelectedPath);
@@ -98,14 +180,17 @@ namespace WindowsFormsApp1
                 {
                     objectList.Add(readDataFromFile(file, fbd.SelectedPath));
                 }
-                MessageBox.Show("Read file successfully");
+                writeExcelFile(fileName, objectList);
+                MessageBox.Show("ファイル集合が完了しました");
                 objectList.Clear();
+                xlApp.Quit();
             }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
